b3b8d28b241af3807483fefef87edc5073db7e2e
sandeep8854 <sandeepstructure@gmail.com>
2021-07-05 23:22:39 +0530

    AC

---
+++ DSA/src/sandeep/Leetcode/com/Leetcode_441_Arranging_Coins.java
@@ -0,0 +1,20 @@
+package sandeep.Leetcode.com;
+import java.util.Scanner;
+public class Leetcode_441_Arranging_Coins
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Any Number  ? ?    ");
+		int num=sc.nextInt();
+		int res=arrangingCoins(num);
+		System.out.println(res);
+	}
+	private static int arrangingCoins(int num)
+	{
+		//Incomplete...
+		
+		return 0;
+	}
+
+}
